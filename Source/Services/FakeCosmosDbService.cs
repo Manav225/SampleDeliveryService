0857d8ebba803b2641d93edb0c7d9478a844c5a1
Manav Sarang <93210770+Manav225@users.noreply.github.com>
2025-04-09 23:55:39 +0530

    Update FakeCosmosDbService.cs

--- Source/Services/FakeCosmosDbService.cs
+++ Source/Services/FakeCosmosDbService.cs
@@ -72,6 +72,146 @@ namespace SampleDeliveryService.Services
                     Longitude = "-122.2015",
                     Completed = true
                 }
+                new Order
+                {
+                    Id = "3",
+                    FirstName = "Aarav",
+                    LastName = "Deshmukh",
+                    Packages = 2,
+                    Street = "101 MG Road",
+                    City = "Pune",
+                    State = "Maharashtra",
+                    ZipCode = 411001,
+                    Latitude = "18.5204",
+                    Longitude = "73.8567",
+                    Completed = false
+                },
+                new Order
+                {
+                    Id = "4",
+                    FirstName = "Neha",
+                    LastName = "Patil",
+                    Packages = 1,
+                    Street = "202 Bandra West",
+                    City = "Mumbai",
+                    State = "Maharashtra",
+                    ZipCode = 400050,
+                    Latitude = "19.0760",
+                    Longitude = "72.8777",
+                    Completed = true
+                },
+                new Order
+                {
+                    Id = "5",
+                    FirstName = "Rahul",
+                    LastName = "Jadhav",
+                    Packages = 4,
+                    Street = "303 Civil Lines",
+                    City = "Nagpur",
+                    State = "Maharashtra",
+                    ZipCode = 440001,
+                    Latitude = "21.1458",
+                    Longitude = "79.0882",
+                    Completed = false
+                },
+                new Order
+                {
+                    Id = "6",
+                    FirstName = "Sneha",
+                    LastName = "Kulkarni",
+                    Packages = 2,
+                    Street = "Shivaji Chowk",
+                    City = "Nashik",
+                    State = "Maharashtra",
+                    ZipCode = 422001,
+                    Latitude = "19.9975",
+                    Longitude = "73.7898",
+                    Completed = false
+                },
+                new Order
+                {
+                    Id = "7",
+                    FirstName = "Vikas",
+                    LastName = "Bhosale",
+                    Packages = 1,
+                    Street = "MIDC Area",
+                    City = "Aurangabad",
+                    State = "Maharashtra",
+                    ZipCode = 431001,
+                    Latitude = "19.8762",
+                    Longitude = "75.3433",
+                    Completed = true
+                },
+                new Order
+                {
+                    Id = "8",
+                    FirstName = "Priya",
+                    LastName = "Naik",
+                    Packages = 3,
+                    Street = "Main Market",
+                    City = "Kolhapur",
+                    State = "Maharashtra",
+                    ZipCode = 416001,
+                    Latitude = "16.7050",
+                    Longitude = "74.2433",
+                    Completed = false
+                },
+                new Order
+                {
+                    Id = "9",
+                    FirstName = "Kiran",
+                    LastName = "Sawant",
+                    Packages = 2,
+                    Street = "Old Bus Stand",
+                    City = "Solapur",
+                    State = "Maharashtra",
+                    ZipCode = 413001,
+                    Latitude = "17.6599",
+                    Longitude = "75.9064",
+                    Completed = true
+                },
+                new Order
+                {
+                    Id = "10",
+                    FirstName = "Sakshi",
+                    LastName = "Shinde",
+                    Packages = 5,
+                    Street = "Jijamata Nagar",
+                    City = "Thane",
+                    State = "Maharashtra",
+                    ZipCode = 400601,
+                    Latitude = "19.2183",
+                    Longitude = "72.9781",
+                    Completed = false
+                },
+                new Order
+                {
+                    Id = "11",
+                    FirstName = "Aniket",
+                    LastName = "Rane",
+                    Packages = 2,
+                    Street = "Panvel Junction",
+                    City = "Navi Mumbai",
+                    State = "Maharashtra",
+                    ZipCode = 410206,
+                    Latitude = "18.9894",
+                    Longitude = "73.1175",
+                    Completed = false
+                },
+                new Order
+                {
+                    Id = "12",
+                    FirstName = "Meera",
+                    LastName = "Bhagat",
+                    Packages = 1,
+                    Street = "Satara Road",
+                    City = "Satara",
+                    State = "Maharashtra",
+                    ZipCode = 415001,
+                    Latitude = "17.6805",
+                    Longitude = "74.0183",
+                    Completed = true
+                }
             };
 
             return Task.FromResult((IEnumerable<Order>)orders);
