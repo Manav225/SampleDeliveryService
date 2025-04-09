5e4fe188e1bb7b902630ee1c59f94f9d7bb92b18
Manav Sarang <93210770+Manav225@users.noreply.github.com>
2025-04-09 15:56:08 +0530

    Update TokenAuthorizationProvider.cs

--- Source/Models/TokenAuthorizationProvider.cs
+++ Source/Models/TokenAuthorizationProvider.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
@@ -9,7 +9,7 @@ namespace SampleDeliveryService.Models
 {
     /// <summary>
     /// This class is used to provide a basic JWT (Json Web Token) security token service which can issue short lived access tokens to be set on the Cookie responses to the website.
-    /// This access token is verfied to be issued from the website and that noone else forged a token. Then an Azure Maps token can be issued.
+    /// This access token is verfied to be issued from the website and that no one else forged a token. Then an Azure Maps token can be issued.
     /// </summary>
     public class TokenAuthorizationProvider
     {
@@ -23,17 +23,10 @@ namespace SampleDeliveryService.Models
             var now = DateTime.UtcNow;
             var signingCredentials = new SigningCredentials(CreateSecurityKey(), SecurityAlgorithms.HmacSha256Signature);
 
-            long jwtTokenId;
-            using (RandomNumberGenerator rng = new RNGCryptoServiceProvider())
-            {
-                var tokenData = new byte[32];
-                rng.GetNonZeroBytes(tokenData);
-                jwtTokenId = BitConverter.ToInt64(tokenData);
-            }
+            var tokenData = new byte[32];
+            RandomNumberGenerator.Fill(tokenData); // ✅ Replaces RNGCryptoServiceProvider
+            long jwtTokenId = BitConverter.ToInt64(tokenData, 0);
 
-            // add a cryptographically secured random number representing the session id.
-            // this session id can be used to throttle or track much traffic is issued.
-            // For additional protection, you can provide client IP address for the case of tracking source location.
             var claims = new List<Claim>() { new Claim("jti", jwtTokenId.ToString()) };
             return jwtTokenHandler.CreateJwtSecurityToken(Issuer, Audience, new ClaimsIdentity(claims), now, now.AddMinutes(10), now, signingCredentials);
         }
@@ -52,9 +45,9 @@ namespace SampleDeliveryService.Models
             {
                 return privateKey;
             }
-            using RandomNumberGenerator rng = new RNGCryptoServiceProvider();
+
             var tokenData = new byte[32];
-            rng.GetBytes(tokenData);
+            RandomNumberGenerator.Fill(tokenData); // ✅ Updated here as well
             privateKey = tokenData;
             return privateKey;
         }
